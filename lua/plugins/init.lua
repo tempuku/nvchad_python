ff17bf3ebc31b0a151bf41bbf05315f81c415377
Aleksandr <avolkov@estee.com>
2024-12-01 18:59:32 +0300

    init

---
+++ lua/plugins/init.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  { "stevanmilic/nvim-lspimport" },
+ { "elentok/format-on-save.nvim",
+  config = function()
+      require "configs.format-on-save"
+    end,
+  },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
